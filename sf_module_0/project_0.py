bd6fd037b86e92676373d2249c3e5a712f0b1857
EvgeniaMi03 <eky7533@gmail.com>
2020-04-06 15:15:51 +0300

    Добавила расчет среднего значения

--- sf_module_0/project_0.py
+++ sf_module_0/project_0.py
@@ -19,3 +19,18 @@ def game_v3(number):
 
 count1=game_v3(number)
 print (f"Вы угадали число {number} за {count1} попыток.")
+
+# Добавила функцию: Повторим 1000 раз, чтоб понять в среднем, насколько быстро решает задачу наш подход.
+def score_game(game_v):
+    '''Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число'''
+    count_ls = []
+    np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
+    random_array = np.random.randint(1, 101, size=(1000))
+    for number in random_array:
+        count_ls.append(game_v(number))
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
+    return(score)
+
+# запускаем
+score_game(game_v3)
